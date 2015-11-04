79881ebe3c6093653972872ed3ca07511d19dd0c
huang <18649700972@163.com>
2015-11-04 16:54:17 +0800

    First commit.

---
+++ src/com/example/myaccount/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myaccount;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+		startActivity(intent);
+		finish();
+	}
+
+}
